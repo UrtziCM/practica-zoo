5e1a1db7c833f3f1bbd9e96ab896e895f3b6fb48
Yxini <urcrmedev@gmail.com>
2025-04-05 11:39:10 +0200

    update: updated constructor, added methods and defined mammal

--- classes/Animales.h
+++ classes/Animales.h
@@ -14,13 +14,19 @@ class Animal {
 			OMNIVORE
 		};
 		
-		Animal();
+		Animal(std::string name, std::string species, int age, SEX sex, DIET diet);
 		virtual ~Animal();
 		std::string GetName();
 		std::string GetSpecies();
 		int GetAge();
 		SEX GetSex();
 		DIET GetDiet();
+
+		void SetName(std::string name);
+		void SetSpecies(std::string species);
+		void SetAge(int age);
+		void SetSex(SEX sex);
+		void SetDiet(DIET diet);
 
 	private:
 		std::string name;
@@ -31,13 +37,9 @@ class Animal {
 };
 
 class Mammal : Animal {
-	private:
-		int legCount;
-		int daysToGest;
-		FUR furType;
 	public:
 		enum FUR {
-			DEFINITIVE,
+			DEFINITIVE = 0,
 			VIBRISSAE,
 			PELAGE,
 			SPINES,
@@ -45,7 +47,26 @@ class Mammal : Animal {
 			VELLI,
 			WOOL
 		};
-		
+
+		Mammal(std::string name, std::string species, int age, SEX sex, DIET diet, int legCount, int daysToGest, FUR furType) : Animal(name, species, age, sex, diet) {
+			this->legCount = legCount;
+			this->daysToGest = daysToGest;
+			this->furType = furType;
+		};
+		~Mammal();
+
+		int GetLegCount();
+		int GetDaysToGest();
+		FUR GetFurType();
+
+		void SetLegCount(int legCount);
+		void SetDaysToGest(int daysToGest);
+		void SetFurType(FUR furType);
+
+	private:
+		int legCount;
+		int daysToGest;
+		FUR furType;
 };
 
 class Reptile : Animal {};
